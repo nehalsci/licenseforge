e8025240cd3fdc12de71be35fe7fb1ea42d43ceb
nehalsci <nehal71.sci@gmail.com>
2024-01-04 22:56:46 +0600

    feat(list): list all the licenses

--- cli/commands/list.js
+++ cli/commands/list.js
@@ -1,3 +1,25 @@
+const fs = require("node:fs");
+
 module.exports = function licenses() {
-	console.log("[WIP] License list");
+	const licenses = readLicenseDirectory();
+	console.log("Licenses:\n--------------");
+
+	licenses.forEach(license => {
+		console.log(`- ${license.name}`);
+		console.log("--------------");
+	});
+}
+
+function readLicenseDirectory() {
+	const files = fs.readdirSync("./licenses");
+	const array = [];
+
+	files.forEach(name => {
+		array.push({
+			name: name.toLowerCase(),
+			originalName: name
+		});
+	});
+
+	return array;
 }
